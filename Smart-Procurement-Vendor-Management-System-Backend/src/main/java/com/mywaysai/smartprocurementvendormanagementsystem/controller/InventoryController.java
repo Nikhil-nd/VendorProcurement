42f26316b0bd6932467213ca58df3f318d33150f
Nikhil-nd <156750385+Nikhil-nd@users.noreply.github.com>
2026-04-13 22:09:52 +0530

    Update InventoryController.java

--- Smart-Procurement-Vendor-Management-System-Backend/src/main/java/com/mywaysai/smartprocurementvendormanagementsystem/controller/InventoryController.java
+++ Smart-Procurement-Vendor-Management-System-Backend/src/main/java/com/mywaysai/smartprocurementvendormanagementsystem/controller/InventoryController.java
@@ -11,7 +11,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/inventory")
-@CrossOrigin(origins = {"http://localhost:5173", "http://localhost:5180"})
+
 @RequiredArgsConstructor
 public class InventoryController {
 
