a492ab80e5f771f42c9c8dfc0ec86f228d2334ca
Nikhil-nd <156750385+Nikhil-nd@users.noreply.github.com>
2026-04-13 22:14:49 +0530

    Update VendorRatingController.java

--- Smart-Procurement-Vendor-Management-System-Backend/src/main/java/com/mywaysai/smartprocurementvendormanagementsystem/controller/VendorRatingController.java
+++ Smart-Procurement-Vendor-Management-System-Backend/src/main/java/com/mywaysai/smartprocurementvendormanagementsystem/controller/VendorRatingController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/vendor-ratings")
-@CrossOrigin(origins = {"http://localhost:5173", "http://localhost:5180"})
+
 public class VendorRatingController {
 
     private final VendorRatingService service;
