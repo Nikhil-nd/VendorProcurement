38b5e78ca3ca62af0123b25137e007da9291f8dc
Nikhil-nd <156750385+Nikhil-nd@users.noreply.github.com>
2026-04-13 22:11:19 +0530

    Update PurchaseOrderController.java

--- Smart-Procurement-Vendor-Management-System-Backend/src/main/java/com/mywaysai/smartprocurementvendormanagementsystem/controller/PurchaseOrderController.java
+++ Smart-Procurement-Vendor-Management-System-Backend/src/main/java/com/mywaysai/smartprocurementvendormanagementsystem/controller/PurchaseOrderController.java
@@ -11,7 +11,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/po")
-@CrossOrigin(origins = {"http://localhost:5173", "http://localhost:5174", "http://localhost:5180"})
+
 @RequiredArgsConstructor
 public class PurchaseOrderController {
 
